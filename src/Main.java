6c00e29b99025ad5fea442a3e6dea5d731f2f8b4
Calipso42 <kia151189@@gmail.com>
2026-07-01 14:42:54 +0700

    Задание 2.2.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,31 @@
 public class Main {
+
     public static void main(String[] args) {
+        // Задание 1
         printThreeWords();
+
+        // Задание 2
+        checkSumSign();
     }
 
+    // Задание 1
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    // Задание 2
+    public static void checkSumSign() {
+        int a = 1;
+        int b = 2;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
